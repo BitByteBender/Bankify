9fa963c36aa8cf88a768966852c76fe8a3877c1c
DevTunechi <tunjidare2@yahoo.com>
2024-10-04 18:42:16 +0100

    To run app locally during development

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import create_app
+from config import Config
+
+# Initialize the app with the configuration settings
+app = create_app(Config)
+
+if __name__ == "__main__":
+    # Run the app in debug mode for development purposes
+    app.run(debug=True, host='0.0.0.0', port=5000)
