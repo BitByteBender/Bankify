ed440c5657fb07e62ea7022f2e0cb498020991ad
DevTunechi <tunjidare2@yahoo.com>
2024-10-04 11:33:23 +0100

    Data Manipulation - Insert/Update/Find/Delete/Hold Users

---
+++ app/services/user_service.py
@@ -0,0 +1,23 @@
+def create_user(data):
+    # Logic to insert a new user into the database
+    pass
+
+def update_user(user_id, data):
+    # Logic to update a user's information
+    pass
+
+def find_user_by_id(user_id):
+    # Logic to retrieve a single user by their ID
+    pass
+
+def find_all_users():
+    # Logic to retrieve all users
+    pass
+
+def delete_user(user_id):
+    # Logic to delete a user from the database
+    pass
+
+def hold_user_account(user_id):
+    # Logic to freeze/hold a user account
+    pass
