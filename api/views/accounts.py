2891bd1147f1e7afbe0df2b826b60d4059f6729c
Soufiane Sadgali <ssadgali2@gmail.com>
2024-10-07 03:13:50 +0100

    Added an endpoint to retrieve all accounts records of a specific user

--- api/views/accounts.py
+++ api/views/accounts.py
@@ -2,6 +2,7 @@
 """ Accounts endpoints(routes) """
 
 from api.views import app_views
+from flask import abort
 from models import storage
 from models.users import User
 from models.accounts import Account
@@ -11,7 +12,7 @@ import json
 USR_PATH = '/users/<user_id>'
 ACC_PATH = '/accounts'
 ACCID_PATH = str(ACC_PATH + '/<account_id>')
-# UA_PATH = str(USR_PATH + ACC_PATH)
+UA_PATH = str(USR_PATH + ACC_PATH)
 
 
 @app_views.route(ACC_PATH, methods=['GET'], strict_slashes=False)
@@ -19,3 +20,11 @@ def get_all_accounts():
     """ Retrieves all bank accounts """ 
     acc_records = storage.all(Account).values()
     return (json.dumps([acc.to_dict() for acc in acc_records], indent=4) + '\n')
+
+
+@app_views.route(UA_PATH, methods=['GET'], strict_slashes=False)
+def get_all_user_accounts(user_id):
+    """ Retrives all Accounts specific to a User """
+    user_record = storage.get(User, user_id) or abort(404, "Not found")
+    return (json.dumps([usr.to_dict() for usr in user_record.accounts],
+            indent=3) + '\n', 200)
