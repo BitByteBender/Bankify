004e54eca7ec7b1cc9633f175971fd60a8982818
Soufiane Sadgali <ssadgali2@gmail.com>
2024-10-07 05:23:42 +0100

    Added a new endpoint for inserting a new account for a specific user

--- api/views/accounts.py
+++ api/views/accounts.py
@@ -2,16 +2,18 @@
 """ Accounts endpoints(routes) """
 
 from api.views import app_views
-from flask import abort
+from flask import abort, request
 from models import storage
 from models.users import User
 from models.accounts import Account
+from sqlalchemy.exc import IntegrityError
+from uuid import uuid4
 import json
 
 
 USR_PATH = '/users/<user_id>'
 ACC_PATH = '/accounts'
-ACCID_PATH = str(ACC_PATH + '/<account_id>')
+ACC_ID_PATH = str(ACC_PATH + '/<account_id>')
 UA_PATH = str(USR_PATH + ACC_PATH)
 
 
@@ -30,8 +32,41 @@ def get_all_user_accounts(user_id):
             indent=4) + '\n', 200)
 
 
-@app_views.route(ACCID_PATH, methods=['GET'], strict_slashes=False)
+@app_views.route(ACC_ID_PATH, methods=['GET'], strict_slashes=False)
 def get_single_account(account_id):
     """ Retrieves a specifc accoung based on its id """
     acc_rec = storage.get(Account, account_id) or abort(404)
     return (json.dumps(acc_rec.to_dict(), indent=4) + '\n', 200)
+
+
+@app_views.route(UA_PATH, methods=['POST'], strict_slashes=False)
+def insert_account(user_id):
+    """ Inserts a new bank account for a specific user """
+    user_obj = storage.get(User, user_id) or abort(404)
+    data = request.get_json() or abort(400, "Not a JSON")
+    balance = data.get('balance') if 'balance' in data else abort(400, "Balance is missing")
+    credits = data.get('credits') if 'credits' in data else abort(400, "Credits is missing")
+    on_hold = data.get('on_hold', False)
+
+    data.update({
+        'id': str(uuid4()),
+        'user_id': user_id,
+        'balance': str(balance),
+        'credits': str(credits),
+        'on_hold': on_hold,
+    })
+
+    try:
+        new_account = Account(**data)
+        print("New account record: ", new_account)
+        storage.new(new_account)
+        storage.save()
+    except IntegrityError as ie:
+        print("IntegrityError: ", ie)
+        storage.rollback()
+        abort(400, "Duplicate entry")
+    except Exception as e:
+        print("Exception: ", e)
+        storage.rollback()
+        abort(500, "Failed to insert a new account due to databassse constraint violation")
+    return (json.dumps(new_account.to_dict()) + '\n', 201)
