e63898f2d99e7b66cb7efe08e0a06b92b126f93d
Soufiane Sadgali <ssadgali2@gmail.com>
2024-10-13 18:19:29 +0100

    Added the functionality to reset users password

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from flask import Blueprint, render_template, request, session, redirect, url_for, jsonify
 from api.auth.login import login
+from api.auth.utils import forgot_password, reset_password
+
 
 auth_bp = Blueprint('auth_bp', __name__)
 
@@ -22,3 +24,15 @@ def dashboard_login_page():
 @auth_bp.route('/dashboard/login', methods=['POST'], strict_slashes=False)
 def dashboard_login():
     return login()
+
+
+@auth_bp.route('/forgot_password', methods=['GET', 'POST'], strict_slashes=False)
+def handle_forgot_password():
+    return forgot_password()
+
+
+@auth_bp.route('/reset_password', methods=['GET', 'POST'], strict_slashes=False)
+def handle_reset_password():
+    if request.method == 'GET':
+        return render_template('reset_password.html')
+    return reset_password()
